4b6f758dc02b1994812182e1d8d73e9b9489f93f
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-16 14:12:05 +0100

    Add import

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
+import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
-
 
 lines = []
 with open('../data/driving_log.csv') as csvfile:
