843cd7796b4c6433208d516d073cf269e13873a8
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-16 14:31:36 +0100

    Less epochs

--- model.py
+++ model.py
@@ -32,7 +32,7 @@ model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, nb_epoch=7, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, nb_epoch=2, shuffle=True)
 
 model.save('model.h5')
 
