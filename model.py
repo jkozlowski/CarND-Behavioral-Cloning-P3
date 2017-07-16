133c6ad94a980d4c0c00a4c5b8908b3a2beb901f
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-16 16:59:48 +0100

    Remove Dropout, augment data

--- model.py
+++ model.py
@@ -15,6 +15,7 @@ with open('../data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
+
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
@@ -23,6 +24,15 @@ for line in lines:
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
+
+augmented_images = []
+augmented_measurements = []
+
+for image,measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(cv2.flip(image, 1))
+    augmented_images.append(image)
+    augmented_measurements.append(measurement*-1.1)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -33,10 +43,10 @@ model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=shape))
 model.add(Convolution2D(6,5,5, activation='relu', border_mode='valid'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2), border_mode='valid'))
-model.add(Dropout(0.25))
+#model.add(Dropout(0.25))
 model.add(Convolution2D(6,5,5, activation='relu', border_mode='valid'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2), border_mode='valid'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(120))
 model.add(Dense(84))
