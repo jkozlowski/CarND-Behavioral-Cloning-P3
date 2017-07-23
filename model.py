42bb4f43cd1e81c93409f0b37258189f26f2a3fd
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-23 21:37:56 +0100

    Change crop again

--- model.py
+++ model.py
@@ -42,7 +42,7 @@ with open(args.data_folder + '/driving_log.csv') as csvfile:
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 # Size of correction when deriving left and right steering measurements
-correction = 0.35
+correction = 0.30
 
 # Size of dropout
 dropout = 0.5
@@ -103,7 +103,7 @@ shape = (160,320,3)
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=shape))
 # Crop the unimportant part of the image
-model.add(Cropping2D(cropping=((55,23), (0,0))))
+model.add(Cropping2D(cropping=((60,23), (0,0))))
 # NVidia
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
