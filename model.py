8c839650f8d9013dfb909cf4adadddc4a1bb7bf9
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-23 22:07:54 +0100

    Change crop again

--- model.py
+++ model.py
@@ -103,7 +103,7 @@ shape = (160,320,3)
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=shape))
 # Crop the unimportant part of the image
-model.add(Cropping2D(cropping=((60,23), (0,0))))
+model.add(Cropping2D(cropping=((40,23), (0,0))))
 # NVidia
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
