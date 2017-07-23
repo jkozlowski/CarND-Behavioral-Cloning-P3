b4a468f99b2ce186587dd6a1e617a31276aa579a
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-23 23:30:19 +0100

    Some tweaks, I really dunno whats happening

--- model.py
+++ model.py
@@ -109,7 +109,7 @@ model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=shape))
 # Crop the unimportant part of the image
 model.add(Cropping2D(cropping=((40,23), (0,0))))
 # Resize to 66x66, as per NVidia paper
-model.add(Lambda(lambda image: ktf.image.resize_images(image, (66, 66))))
+model.add(Lambda(lambda image: ktf.image.resize_images(image, 66, 66)))
 # NVidia
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
