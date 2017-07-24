c68a125ba720d01383b776541bfcdd223bb8ac92
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-24 20:39:53 +0100

    Some tweaks, I really dunno whats happening

--- model.py
+++ model.py
@@ -14,8 +14,7 @@ import random
 from random import shuffle
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt 
-from tensorflow import image as tf_image
+import matplotlib.pyplot as plt
 
 weights_file = 'model.h5'
 
@@ -107,10 +106,6 @@ shape = (66,66,3)
 # Create the model
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=shape))
-# # Crop the unimportant part of the image
-# model.add(Cropping2D(cropping=((40,23), (0,0))))
-# Resize to 66x66, as per NVidia paper
-# model.add(Lambda(lambda image: tf_image.resize_images(image, (66, 66))))
 # NVidia
 model.add(Convolution2D(24,5,5, border_mode='valid', subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36,5,5, border_mode='valid', subsample=(2,2), activation='relu'))
