374b154a9d4e9d52c346f66771d157a8c9d0de7c
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-24 21:08:34 +0100

    Some tweaks, I really dunno whats happening

--- model.py
+++ model.py
@@ -60,7 +60,7 @@ def get_image(path):
     filename = path.split('/')[-1]
     current_path = args.data_folder + '/IMG/' + filename
     image = cv2.imread(current_path);
-    return cv2.resize(image[23:140,:], (66,66))
+    return cv2.resize(image[40:140,:], (66,66))
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
